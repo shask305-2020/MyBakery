ec195998bcff18e0233e161342a5f72ad300b48d
anton <shask305@yandex.ru>
2024-02-10 22:50:24 +0500

    Version 0.0.3

---
+++ app/src/main/java/com/applife/mybakery/network/BakeryApiService.kt
@@ -0,0 +1,23 @@
+package com.applife.mybakery.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "http://c92267r3.beget.tech"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface BakeryApiService {
+    @GET("clients")
+    fun getClient(): String
+}
+
+object BakeryApi {
+    val retrofitService : BakeryApiService by lazy {
+        retrofit.create(BakeryApiService::class.java)
+    }
+}
